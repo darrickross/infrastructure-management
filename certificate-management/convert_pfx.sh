e7dec7a80b05af5108dc1219a7bcc8d96d0667fb
Darrick Ross <dross56@jhu.edu>
2024-08-18 20:21:35 -0400

    Argument parsing

--- certificate-management/convert_pfx.sh
+++ certificate-management/convert_pfx.sh
@@ -71,3 +71,134 @@ RESET='\033[0m'  # No Color
 print_error() {
   echo -e "${RED}ERROR${RESET}: $1"
 }
+
+# ------------------------------------------------------------------------------
+# Parse Command Line Arguments
+# ------------------------------------------------------------------------------
+
+# Default Required Arguments
+pfx_file=""
+
+# Default Optional File Types to extract arguments
+extract_key=false
+extract_pub=false
+extract_cert=false
+extract_chain=false
+extract_root_ca=false
+
+# Default Optional modifiers arguments
+output_name=""
+display_details=false
+log_level=1
+
+while [[ "$#" -gt 0 ]]; do
+  case "$1" in
+  # Required Options
+  -i | --input)
+    pfx_file="$2"
+    shift 2
+    ;;
+
+  # Optional File Types to extract
+  -k | --key)
+    extract_key=true
+    shift
+    ;;
+  -p | --pub)
+    extract_pub=true
+    shift
+    ;;
+  -C | --cert)
+    extract_cert=true
+    shift
+    ;;
+  -c | --chain)
+    extract_chain=true
+    shift
+    ;;
+  -r | --root-ca)
+    extract_root_ca=true
+    shift
+    ;;
+
+  -s | --standard)
+    extract_key=true
+    extract_cert=true
+    extract_chain=true
+    shift
+    ;;
+  -a | --all)
+    extract_key=true
+    extract_pub=true
+    extract_cert=true
+    extract_chain=true
+    extract_root_ca=true
+    shift
+    ;;
+
+  # Optional modifiers
+  -o | --output)
+    output_name="$2"
+    shift 2
+    ;;
+  -d | --details)
+    display_details=true
+    shift
+    ;;
+  -q | --quiet)
+    log_level=0
+    shift
+    ;;
+  -v | --verbose)
+    log_level=2
+    shift
+    ;;
+  -h | --help)
+    show_help
+    exit 0
+    ;;
+  *)
+    show_help
+    print_error "Unknown option: $1"
+    exit 1
+    ;;
+  esac
+done
+
+# ------------------------------------------------------------------------------
+# Validate arugments
+# ------------------------------------------------------------------------------
+
+if [ -z "$pfx_file" ]; then
+  show_help
+  print_error "Missing required argument: --input/-i"
+  exit 1
+fi
+
+if [ ! -f "$pfx_file" ]; then
+  print_error "File not found: $pfx_file"
+  exit 1
+fi
+
+# ------------------------------------------------------------------------------
+# Print Selected Options
+# ------------------------------------------------------------------------------
+
+if [ "$log_level" -gt 0 ]; then
+  cat <<EOF
+  Selected Options:
+    Input .pfx file:  $pfx_file
+    Output base name: $output_name
+
+  Keys to extract:
+    - Extract private key:          $extract_key
+    - Extract public key:           $extract_pub
+    - Extract certificate:          $extract_cert
+    - Extract chain certificate:    $extract_chain
+    - Extract root CA certificate:  $extract_root_ca
+
+  Modifiers:
+    - Display details: $display_details
+    - Log level:       $log_level
+EOF
+fi
